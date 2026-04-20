55d8c849dc4c75afddb6aefe835aba9f3392e9b6
ycchuang100 <ycchuang10@gmail.com>
2026-04-20 11:21:51 +0800

    Add files via upload

--- daily-poem.js
+++ daily-poem.js
@@ -45,7 +45,7 @@ function generatePoem() {
 
     const postData = JSON.stringify({
       contents: [{ parts: [{ text: prompt }] }],
-      generationConfig: { temperature: 0.8, maxOutputTokens: 1000 }
+      generationConfig: { temperature: 0.8, maxOutputTokens: 2000 }
     });
 
     const options = {
